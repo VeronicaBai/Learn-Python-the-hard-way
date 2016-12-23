82080dd13d4c7e0493dbcf2323f71ca0ba44df30
白璐 <bailu@bailudeMacBook-Air.local>
2016-12-23 19:08:44 +0800

    add new file

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#encoding=utf-8
+print "Hellow World!"
+print "Hellow Again"
+print "This is fun."
+print "你好"
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said"do not touch this.'
